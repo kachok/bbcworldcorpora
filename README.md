fb2fe70be3ae7bd9dca5250b34c552948dedfd44
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2012-04-12 15:33:29 -0400

    basic text dump of articles

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
+BBC World Service Corpora
+=========================
+
+This is Python library to scrape BBC World Service website for foreign languages and build related corpora of articles.
+
+Tested on:
+* Russian
+* Tamil
+* Urdu
+
 Dependencies
 ------------
 
-feedreader
+feedparser 5.1.1
--- bbcworldcorpora/__init__.py
+++ bbcworldcorpora/__init__.py
@@ -1,4 +1,7 @@
 import feedparser
+import urllib
+
+from lxml.html import parse
 
 #return feed object
 def get_feed(lang):
@@ -8,8 +11,29 @@ def get_feed(lang):
 #return list of articles
 def get_articles(lang):
 	feed=get_feed(lang)
+	links=[]
 	for entry in feed.entries:
-		print entry['title']
-		
+		#print entry['title']
+		#print entry['link']
+		links.append(entry['link'])
+
+	return links
+
+def get_article(url):
+	print "-----------------------------------"
+	print url
+	doc = parse(url).getroot()
+	try:
+		body=doc.cssselect('div.story-body')[0]
+		print body.text_content()
+	except:
+		print 'no story-body'
+
+			
 if __name__ == '__main__':
-	get_articles('russian')
+	links = get_articles('russian')
+	links = get_articles('urdu')
+	links = get_articles('tamil')
+	for link in links:
+		#link=links[0]
+		get_article(link)
